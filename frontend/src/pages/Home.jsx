4b81a772b5ce6a2f4e67a2e6cccf114196c6bdbd
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-03 14:10:48 +0200

    panier wip

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect ,useState} from 'react';
 import { useUserContext } from "../context/UserContext";
-import callApi from "../Services/CallApi";
+import callApi from " ../Services/CallApi";
 import ProductCard from '../Components/productCard';
 function home() {
 
@@ -23,20 +23,24 @@ function home() {
 //     })
 //   })
  return (
-<div className='flex flex-col '>
-    
+<div className='flex flex-col'>
   <div className='text-xl'>Bienvenue {userName}</div>
   <div>
-    { product.map((product) => (
-      <ProductCard
-        key={product.id}
-        photo={product.photo}
-        price={product.price}
-        title={product.title} />
+    { Array.isArray(product) && product.length > 0 ? (
+      product.map((product) => (
+        <ProductCard
+          key={product.id}
+          photo={product.photo}
+          price={product.price}
+          title={product.title} />
         // {/* <button type="button" onClick={handleAdd}>ajouter au panier</button> */}
-     
-    ))}
-  </div> </div>
+      ))
+    ) : (
+      <p>No products found.</p>
+    )}
+  </div>
+</div>
+
   
 )
 }
--- frontend/src/pages/Panier.jsx
+++ frontend/src/pages/Panier.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {useUserContext} from "../context/UserContext"
+
 
 function panier() {
+
+  const { userId } = useUserContext();
+  //je veux recuperer le dernier cart du user
+  //
   return (
     <div>panier</div>
   )
