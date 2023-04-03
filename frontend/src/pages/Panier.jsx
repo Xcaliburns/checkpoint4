6557b7d964b5911fe08a701fa6473b8f5436019b
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-03 17:01:22 +0200

    panier recup produit ok

--- frontend/src/pages/Panier.jsx
+++ frontend/src/pages/Panier.jsx
@@ -5,6 +5,7 @@ import CallApi from "../Services/CallApi"
 
 function panier() {
   const [cart,setCart]=useState([]);
+  const [cartproductlist,setCartProductList]=useState([]);
   const { userId } = useUserContext();
   //je veux recuperer le dernier cart du user:
   //----il me faut tout les carts
@@ -19,6 +20,17 @@ function panier() {
     .catch((err) => console.error(err));
 }, []);
 
+   useEffect(() => {
+  CallApi.get("/api/cartproduct/")
+    .then((res) => {console.log(res.data)
+      const filteredData = res.data.filter(item => item.cart_id === cart.id);
+      console.log(filteredData);
+    setCartProductList(res.data)
+      //  console.log(userId)
+    })
+    .catch((err) => console.error(err));
+}, []);
+
 
 console.log(cart)
 
@@ -26,8 +38,10 @@ console.log(cart)
   //-----il me faut product cart qui ont le meme id que les cart du user 
   //je veux recuperer les articles du panier (product_cart)
   return (
-    <div>panier</div>
+    <div>
+        { cartproductlist.map((product) =><div>{product.title}{product.price} </div>)}
+    </div>
   )
 }
 
-export default panier
+export default panier;
